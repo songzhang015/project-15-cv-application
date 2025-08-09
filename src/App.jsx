78549da94ae32d242ea064caf575a14ba99cd27b
Song <songzhang015@gmail.com>
2025-08-08 19:50:58 -0700

    Set bg color and reset more of the preset

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
-  return (
-    <div className="app">
-      <h1>CV Application</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-      </div>
-    </div>
-  )
+    return (
+        <div className="app">
+        
+        </div>
+    )
 }
 
 export default App

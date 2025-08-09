b5fa91ed7bb5db87ca59efb8edadb53307ac2fba
Song Zhang <songzhang015@gmail.com>
2025-08-09 16:41:35 -0700

    Adjust section active colors and start text input

--- src/components/Inputs/SectionTabs.jsx
+++ src/components/Inputs/SectionTabs.jsx
@@ -9,26 +9,28 @@ function SectionTabs({ activeTab, onTabChange }) {
         <div className="section-tabs">
             <button
                 onClick={() => onTabChange("general")}
-                className="section-tab"
+                className={`section-tab ${activeTab === "general" ? "active" : ""}`}
             >
                 General
             </button>
 
             <button
                 onClick={() => onTabChange("experience")}
-                className="section-tab"
+                className={`section-tab ${activeTab === "experience" ? "active" : ""}`}
             >
                 Experience
             </button>
+
             <button
                 onClick={() => onTabChange("education")}
-                className="section-tab"
+                className={`section-tab ${activeTab === "education" ? "active" : ""}`}
             >
                 Education
             </button>
+
             <button
                 onClick={() => onTabChange("skills")}
-                className="section-tab"
+                className={`section-tab ${activeTab === "skills" ? "active" : ""}`}
             >
                 Skills
             </button>
--- src/components/fields/TextInput.jsx
+++ src/components/fields/TextInput.jsx
@@ -1,1 +1,11 @@
 // TextInput.jsx
+
+import { useState } from "react";
+import "../../styles/Inputs.css";
+import SectionTabs from "./SectionTabs";
+
+function Inputs({ label, placeholder, value, onChange }) {
+    return <div className="inputs"></div>;
+}
+
+export default Inputs;

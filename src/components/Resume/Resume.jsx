f42efc1b60735a5c401e464687550ae71e787b9f
Song <songzhang015@gmail.com>
2025-08-10 13:11:15 -0700

    Start resume section and formatting (does not currently use inputs

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,12 +1,58 @@
 // Resume.jsx
 
-import { useState } from "react";
 import "../../styles/Resume.css";
 
 function Resume() {
-    // const [count, setCount] = useState(0)
+	return (
+		<div className="resume">
+			<header className="resume-header">
+				<h1>Title</h1>
+				<div className="line-header">
+					<h2>Email</h2>
+					<h2>Phone</h2>
+					<h2>Address</h2>
+				</div>
+			</header>
 
-    return <div className="resume"></div>;
+			<div className="resume-summary">
+				<p>Summary</p>
+			</div>
+
+			<div className="resume-experience">
+				<div className="resume-banner">
+					<h2>Work Experience</h2>
+				</div>
+				<div className="line">
+					<h2>Title</h2>
+					<h2>Start Date - End Date</h2>
+				</div>
+				<h2>Company</h2>
+				<h2>Address</h2>
+				<p>Description</p>
+			</div>
+
+			<div className="resume-education">
+				<div className="resume-banner">
+					<h2>Education</h2>
+				</div>
+				<div className="line">
+					<h2>Degree</h2>
+					<h2>Start Date - End Date</h2>
+				</div>
+				<h2>University</h2>
+				<h2>Address</h2>
+			</div>
+
+			<div className="resume-skills">
+				<div className="resume-banner">
+					<h2>Skills</h2>
+				</div>
+				<div>
+					<p>Skill 1: Desc</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Resume;
